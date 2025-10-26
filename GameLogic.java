3101a5c843fb2971a2c08f2d3ca995ce6461e30f
pardis-21 <pardisehsani@cmail.carleton.ca>
2025-10-26 13:49:13 -0400

    working on game logic and the players and the cards

--- GameLogic.java
+++ GameLogic.java
@@ -23,15 +23,43 @@ public class GameLogic{
 
         }
 
-        cards = new ArrayList<>();
         hand = new ArrayList<>();
         discardPile = new ArrayList<>();
-        flipPile = new ArrayList<>();
-        drawPile = new ArrayList<>();
 
+        flipPile = new ArrayList<>();
+
+        //populating the cards with a card to make a deck (108 cards)
+        drawPile = new ArrayList<>();
+        for (int i = 0; i <= 108; i++){
+            drawPile.add( new Card());
+        }
+
+        //assuming by UNO rules that all players have same age and strating from CW direction
         direction = true; //clockwise direction
         score = 0;
     }
+
+    //at the beginning of the game, each player is dealt 7 cards
+    private void dealCardsBeginning(){
+        for (Player player : players) {
+            if (player.getHand().size() == 7){
+                break;
+            }
+            else {
+                for (Card card : drawPile){
+                    player.getHand().add(card);
+
+            }
+
+            }
+        }
+    }
+
+    private void addCardToPlayerHand(){
+
+
+    }
+
 
 
 
