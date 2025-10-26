3b6d6be039a9b249e80533c437aefbb31a6507cb
pardis-21 <pardisehsani@cmail.carleton.ca>
2025-10-26 12:16:47 -0400

    gamelogic

---
+++ GameLogic.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class GameLogic {
+    private ArrayList<Player> players;
+    private ArrayList<Card> cards;
+    private ArrayList<Card> hand;
+
+    public GameLogic() {
+
+    }
+
+
+}
