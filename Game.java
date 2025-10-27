a187f5049c8fba8121ff00e93d525ab7e7c687fa
Anvita Ala <anvita632.a@gmail.com>
2025-10-27 15:05:13 -0400

    cant have players of same name, no crashing when non-int numPlayers

--- Game.java
+++ Game.java
@@ -2,19 +2,30 @@ import java.util.Scanner;
 import java.util.*;
 
 public class Game {
+    static ArrayList<String> players;
+
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
         int numPlayers = 0;
+        players = new ArrayList<>();
 
         while (true) {
             System.out.println("Welcome to UNO");
             System.out.println("---------------------------------------");
             System.out.println("Enter the number of Players (2-4): ");
-            numPlayers = userInput.nextInt();
-            System.out.println("");
 
-            ArrayList<String> players = new ArrayList<>();
-            PlayerOrder playerOrder = new PlayerOrder();
+                try {
+                    numPlayers = userInput.nextInt();
+                    userInput.nextLine(); // clear the buffer
+
+                } catch (InputMismatchException e) {
+                    System.out.println("Invalid input! Please enter a number between 2 and 4.\n");
+                    userInput.nextLine(); // clear the invalid input
+                    continue;
+                }
+
+
+        PlayerOrder playerOrder = new PlayerOrder();
 
             if (numPlayers > 4 || numPlayers < 2) {
                 System.out.println("Invalid number of Players!");
@@ -23,12 +34,17 @@ public class Game {
             }
             else {
                 //valid number of players
-                for (int i = 1; i < numPlayers + 1; i++){
-                   System.out.println("Enter the player " + i + " name: ");
+                while (players.size() < numPlayers){
+                   System.out.println("Enter the player name: ");
                    String playerName = userInput.next();
+                   if (players.contains(playerName)){
+                       System.out.println("That player already exists.");
+                       continue;
+                   }
                    players.add(playerName);
                    Player player = new Player(playerName);
                    playerOrder.addPlayer(player);
+
 
                 }
             }
