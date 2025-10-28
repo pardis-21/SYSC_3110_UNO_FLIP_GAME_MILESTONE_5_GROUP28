bcb19986ae3143b499b7850b5f7c81f1767b1750
Cherie <pulcheriembaye11@gmail.com>
2025-10-27 21:53:10 -0400

    Added JavaDocs

--- Game.java
+++ Game.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 import java.util.*;
-
+/**
+ * The Game class runs the main UNO game.
+ * It handles player setup, game rounds, and determines the winner
+ * once a player reaches 500 points.
+ * This class uses PlayerOrder and Gamelogic to manage turns and rules.
+ */
 public class Game {
     static ArrayList<String> players;
 
